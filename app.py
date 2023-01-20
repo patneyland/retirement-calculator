1ed33ee33834964537c1e02a7c610ae5ca783901
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-20 10:00:26 -0700

    changed app title

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-st.title('A Simple Streamlit Web App')
+st.title('Retirment Calculator')
 name = st.text_input('Enter your name', '')
 st.write(f'Hello {name}!')
 x = st.slider('Select an integer x', 0, 10, 1)

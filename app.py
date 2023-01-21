bf6602dca52f611384b1acc80c5fa65ed9bbdd89
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-20 22:46:11 -0700

    added pmt input

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import pandas as pd
-#import numpy_financial as npf
+import numpy_financial as npf
 
 st.title('Retirment Calculator')
 age = st.number_input('Enter your age', 10,110,25)
 retirement_age = st.number_input('Enter your expected retirement age', 10,110,65)
 income = st.number_input("Enter you desired income in retirement in today's dollars", 1000, 10000000,70000, 1000)
+pmt = st.number_input('Enter monthly contribution', 100,1000000,100)
 
 #st.write(f'Hello {name}!')
-x = st.slider('Select an integer x', 0, 10, 1)
+x = npf.pv(.1,retirement_age-age,pmt)
 y = st.slider('Select an integer y', 0, 10, 1)
-df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
+df = pd.DataFrame({'PV': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
 st.write(df)

309102329c074236f40a86983b78aa4437175385
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-20 22:24:13 -0700

    Added age input

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy_financial as npf
+
 st.title('Retirment Calculator')
 name = st.text_input('Enter your name', '')
+age = st.number_input('Enter your Age', 10,110)
+
 st.write(f'Hello {name}!')
 x = st.slider('Select an integer x', 0, 10, 1)
 y = st.slider('Select an integer y', 0, 10, 1)

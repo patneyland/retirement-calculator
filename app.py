04ce9ee0fbdb36e71c234e49e7cf245eaeaed0a3
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-20 22:26:29 -0700

    Changed packages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy_financial as npf
+#import numpy_financial as npf
 
 st.title('Retirment Calculator')
 name = st.text_input('Enter your name', '')

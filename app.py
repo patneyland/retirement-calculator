4c20605e09fc37f33555c1795f157a7d0270176e
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-20 22:34:01 -0700

    Added step value to income

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.title('Retirment Calculator')
 age = st.number_input('Enter your age', 10,110,25)
 retirement_age = st.number_input('Enter your expected retirement age', 10,110,65)
-income = st.number_input("Enter you desired income in retirement in today's dollars", 1000, 10000000,70000)
+income = st.number_input("Enter you desired income in retirement in today's dollars", 1000, 10000000,70000, 1000)
 
 #st.write(f'Hello {name}!')
 x = st.slider('Select an integer x', 0, 10, 1)

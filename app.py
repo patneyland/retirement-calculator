a4fa1814071dab4860d735b3204f3fec633d3392
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-20 22:29:34 -0700

    Added expected retirement age

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pandas as pd
 #import numpy_financial as npf
 
 st.title('Retirment Calculator')
-name = st.text_input('Enter your name', '')
-age = st.number_input('Enter your Age', 10,110)
+age = st.number_input('Enter your age', 10,110,25)
+retirement_age = st.number_input('Enter your expected retirement age', 10,110,65)
 
-st.write(f'Hello {name}!')
+#st.write(f'Hello {name}!')
 x = st.slider('Select an integer x', 0, 10, 1)
 y = st.slider('Select an integer y', 0, 10, 1)
 df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])

8b180a52b1b371ecdbd48115a7f4fd6668800b69
Patrick Neyland <patrick.neyland75@gmail.com>
2023-01-28 19:12:18 -0700

    commented out a package that was not used

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy_financial as npf
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 

f1ccf3a6fe40f198e510e4a9e1073c1c8187687c
Patrick Neyland <patrick.neyland75@gmail.com>
2023-02-17 17:17:36 -0700

    added assumtion explanations and tax variables

--- app.py
+++ app.py
@@ -3,9 +3,35 @@ import pandas as pd
 import numpy_financial as npf
 
 #setting up the web application
-st.title('Retirment Calculator')
-st.write("Checkout the Github [Repository](https://github.com/patrickneyland/retirement-calculator)")
+st.title('A Better Retirment Calculator')
+st.write("Checkout the Github [Repository](https://github.com/patrickneyland/retirement-calculator) for this calculator.")
 
+
+
+
+st.write("This calculator is designed to help you determine how much you need to save for retirement \
+    and how much you can withdraw from your nestegg each month.")
+
+my_expander = st.expander(label='Click here to expand explanations of the various assumptions')
+with my_expander:
+    st.write("Rate of Return - The starting rate of return is based on the historical average of the S&P 500. If you invested money in the S&P500 in 1928, the money you would have today would reflect a 6.6% annual rate of return.")
+    st.write("Inflation - The inflation rate in the United States averaged 3.29 percent from 1914 until 2023")
+    st.write("Taxes - ")
+    st.write("Income in Retirement - ")
+
+advanced_parameters = st.expander(label="Click here to adjust advanced parameters such as tax brackets")
+with advanced_parameters:
+
+    cola1, cola2 = st.columns(2)
+    with cola1:
+    #calculating taxes based on retirement income
+        rate_1 = st.number_input("rate 1", 0,100,25,1)/100
+        rate_2 = st.number_input("rate 2", 0,100,30,1)/100
+        rate_3 = st.number_input("rate 3", 0,100,35,1)/100
+    #income limits for tax brackets
+    with cola2:
+        limit_1 = st.number_input("limit 1", 0,10000000,50000,1000)
+        limit_2 = st.number_input("limit 2", 0,10000000,100000,1000)
 
 col1, col2, col3 = st.columns(3)
 
@@ -20,15 +46,15 @@ with col2:
 
 with col3:
     rate = st.number_input("Expected rate of return", 0.0,100.00,6.60,0.10)/100
-    inflation = st.number_input("Expected inflation rate", 0.0,100.00,2.8,0.10)/100
+    inflation = st.number_input("Expected inflation rate", 0.0,100.00,3.3,0.10)/100
 
-#calculating taxes based on retirement income
-rate_1 = .25
-rate_2 = .30
-rate_3 = .35
-#income limits for tax brackets
-limit_1 = 50000
-limit_2 = 100000
+# #calculating taxes based on retirement income
+# rate_1 = .25
+# rate_2 = .30
+# rate_3 = .35
+# #income limits for tax brackets
+# limit_1 = 50000
+# limit_2 = 100000
 
 real_rate = (1+rate)/(1+inflation)-1
 monthly_withdrawal_amount = withdrawal_amount/12
@@ -50,6 +76,3 @@ st.write("You are currently expecting to work for {} more years".format(work_yea
 st.write("To meet retirment income expectations, your nestegg target is ${:,.0f}".format(nestegg))
 st.write("You will need to invest ${:,.2f}".format(monthly_contributions)+" at the end of each month to reach your nestegg goal.")
 st.write("You will be able to withdraw ${:,.2f}".format(monthly_withdrawal_amount)+" of todays dollars per month after taxes are paid.")
-
-
-
